fbdb84a37176fb5917a7fdf7bb865b2f868854f2
GerTral <alejandro3mm@gmail.com>
2025-09-16 01:19:16 -0500

    feat: reporte model
    new model for "reporte"

---
+++ backend/models/reporte.js
@@ -0,0 +1,42 @@
+/**
+ * This module provides functionality for "reporte" management operations.
+ * 
+ * It exports an object with methods to retrieve, create, and validate client records.
+ * 
+ * @module reporte
+ * @author German Marcillo
+ */
+
+const Reporte = {
+
+    all: async() => {
+        const { data, error } = await supabase.from('reporte').select('*');
+
+        if (error) throw error;
+        return data;
+    },
+
+    create: async(userData) =>{
+        const { data, error } = await supabase.from('reporte').insert([{userData}]).select('*');
+    
+        if (error) throw error;
+        return data[0];
+    },
+
+    delete: async (id_reporte) => {
+        const { error } = await supabase.from('reporte').delete().eq('id_reporte', id_reporte);
+
+        if (error) throw error;
+        return true;
+    },
+
+    edit_report : async (id_reporte, newDescripcion) => {
+        const { data, error } = await supabase.from('reporte').update({descripcion: newDescripcion}).eq('id_reporte', id_reporte).select('*');
+
+        if (error) throw error;
+        return data[0];
+  }
+
+};
+
+module.exports = Reporte;
