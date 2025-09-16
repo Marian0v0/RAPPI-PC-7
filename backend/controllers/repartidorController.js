4ad68a1543e4365047a158323de37f2fd447f7eb
ShCalderon <secalderon222@umariana.edu.co>
2025-09-16 08:18:30 -0500

    feat: Creacion controlador de repartidor
    Funciones agregadas:
    
    - Aceptar pedido
    - Rechazar pedido

---
+++ backend/controllers/repartidorController.js
@@ -0,0 +1,24 @@
+const {Router} = require('express');
+const router = Router();
+const repartidorModel = require('../models/repartidorModel');
+
+router.patch('/aceptarPedido', async (req, res) => {
+    try {
+        const result = await repartidorModel.aceptarPedido(req.body.id_pedido, req.body.id_repartidor);
+        res.json(result);
+    } catch (error) {
+        console.error("❌ Error al actualizar el estado de aceptación", error);
+        res.status(500).json({error: 'Error interno del servidor' });        
+    }
+});
+
+router.patch('/rechazarPedido', async (req, res) => {
+    try {
+        const result = await repartidorModel.rechazarPedido(req.body.id_pedido, req.body.id_repartidor);
+        res.json(result);
+    } catch (error) {
+        console.error("❌ Error al actualizar el estado de rechazo", error);
+        res.status(500).json({error: 'Error interno del servidor' });        
+    }
+});
+
