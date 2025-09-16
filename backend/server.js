0d6057d059dd1fdfa3436a691a0e8bd1356e031e
Atrallor <alejandro3mm@gmail.com>
2025-09-16 09:39:44 -0500

    para que servira xd

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const reporteRoutes = require('./routes/reporteRoutes');
+//const clienteRoutes = require('./routes/client.routes.js');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,6 +17,12 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/backend', reporteRoutes);
+
+// Manejo de errores global
+app.use((err, req, res, next) => {
+  console.error('Error global:', err);
+  res.status(500).json({ error: err.message });
+});
 
 app.listen(PORT, () => {
   console.log(`Microservicio ejecutándose en puerto ${PORT}`);
--- backend/utils/database.js
+++ backend/utils/database.js
@@ -9,4 +9,7 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
+console.log('SUPABASE_KEY:', process.env.SUPABASE_KEY);
+
 module.exports = supabase;
