443a82124c56673db8f3d67c24a2f42f7e6d9e4e
GerTral <alejandro3mm@gmail.com>
2025-09-16 01:44:30 -0500

    feat: reporteController (get reportes with id, cliente, type and all)

---
+++ backend/controllers/reporteController.js
@@ -0,0 +1,65 @@
+/**
+ * This module handles HTTP requests related to "reporte" management operations.
+ * <p>
+ * It exports an object with methods to handle requests for retrieving, creating, and authenticating clients.
+ * 
+ * @module reporteController
+ * @author German Marcillo
+ */
+
+const reporte = require('../models/reporte');
+
+const reporteController = {
+
+    getReportes: async (req, res) => {
+        try {
+            const reportes = await reporte.all();
+            res.json({ success: true, data: reportes });
+        
+        }catch (error) {
+            res.status(500).json({ success: false, error: error.message });
+        }
+    },
+
+    getReportesById: async (req, res) => {
+        try {
+
+            const {id_reporte} = req.body;
+
+            const reportes = await reporte.getById(id_reporte);
+            res.json({ success: true, data: reportes });
+        
+        }catch (error) {
+            res.status(500).json({ success: false, error: error.message });
+        }
+    },
+
+    getReportesByCliente: async (req, res) => {
+        try {
+
+            const {id_cliente} = req.body;
+
+            const reportes = await reporte.getClientReports(id_cliente);
+            res.json({ success: true, data: reportes });
+        
+        }catch (error) {
+            res.status(500).json({ success: false, error: error.message });
+        }
+    },
+
+    getReportesByType: async (req, res) => {
+        try {
+
+            const {tipo_reporte} = req.body;
+
+            const reportes = await reporte.getByType(tipo_reporte);
+            res.json({ success: true, data: reportes });
+        
+        }catch (error) {
+            res.status(500).json({ success: false, error: error.message });
+        }
+    },
+
+};
+
+module.exports = reporteController;
--- backend/models/reporte.js
+++ backend/models/reporte.js
@@ -16,6 +16,27 @@ const Reporte = {
         return data;
     },
 
+    getById: async(id_reporte) =>{
+        const { data, error } = await supabase.from('reporte').select('*').eq('id_reporte', id_reporte);
+
+        if (error) throw error;
+        return data;
+    },
+
+    getClientReports: async(id_cliente) =>{
+        const { data, error } = await supabase.from('reporte').select('*').eq('id_cliente', id_cliente);
+
+        if (error) throw error;
+        return data;
+    },
+
+    getByType: async(tipo_reporte) =>{
+        const { data, error } = await supabase.from('reporte').select('*').eq('tipo_reporte', tipo_reporte);
+
+        if (error) throw error;
+        return data;
+    },
+
     create: async(userData) =>{
         const { data, error } = await supabase.from('reporte').insert([{userData}]).select('*');
     
