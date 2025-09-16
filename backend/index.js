fd8d8b23f594d753a458a7009bda4528a933a0bc
GerTral <alejandro3mm@gmail.com>
2025-09-16 00:19:44 -0500

    feat: Database Connection && Server Init
    remove pruebas.js for architecture and build database connection

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,21 @@
-console.log("ci")
+/**
+ * Server Initialization
+ * @author German Marcillo
+ */
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const reporteRoutes = require('./routes/reporteRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/backend', reporteRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Microservicio ejecutándose en puerto ${PORT}`);
+});
