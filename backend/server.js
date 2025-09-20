84284035b05677e4bb6c900979752a3869337aa1
GerTral <alejandro3mm@gmail.com>
2025-09-20 15:19:50 -0500

    fix: no se que tenian en el main xd que hacian los node y el .env subido

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const reporteRoutes = require('./routes/reporteRoutes');
+const clientRoutes = require('./routes/client.routes');
+const comercioRoutes = require('./routes/comercio.routes');
+const productoRoutes = require('./routes/productoRoutes');
+const restaranteRoutes = require('./routes/restaurandeRoutes');
+const repartidorRoutes = require('./routes/repartidorRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,6 +20,11 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/backend', reporteRoutes);
+app.use('/backend', clientRoutes);
+app.use('/backend', comercioRoutes);
+app.use('/backend', productoRoutes);
+app.use('/backend', restaranteRoutes);
+app.use('/backend', repartidorRoutes);
 
 app.listen(PORT, () => {
   console.log(`Microservicio ejecutándose en puerto ${PORT}`);
