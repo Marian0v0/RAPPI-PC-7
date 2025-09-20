5753c83c97845a00ce3d87d5f82a454fb660220a
NingJjwo <133249196+NingJjwo@users.noreply.github.com>
2025-09-20 14:05:08 -0500

    feat: Added endpoints for client component

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const clientRoutes = require('./routes/client.routes');
 const reporteRoutes = require('./routes/reporteRoutes');
 
 const app = express();
@@ -14,6 +15,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+app.use('/backend', clientRoutes);
 app.use('/backend', reporteRoutes);
 
 app.listen(PORT, () => {
