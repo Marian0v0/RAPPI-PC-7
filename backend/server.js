cc64709ed4f0d6d9a3951d817c592ee04f02b6ce
GerTral <alejandro3mm@gmail.com>
2025-09-20 15:32:08 -0500

    fix: rutas del jovenazo

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const reporteRoutes = require('./routes/reporteRoutes');
-const clientRoutes = require('./routes/client.routes');
 const comercioRoutes = require('./routes/comercio.routes');
 const productoRoutes = require('./routes/productoRoutes');
 const restaranteRoutes = require('./routes/restaurandeRoutes');
 const repartidorRoutes = require('./routes/repartidorRoutes');
-
+const clienteRoutes = require('./routes/client.routes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -20,10 +19,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/backend', reporteRoutes);
-app.use('/backend', clientRoutes);
-app.use('/backend', comercioRoutes);
-app.use('/backend', productoRoutes);
-app.use('/backend', restaranteRoutes);
+app.use('/backend', clienteRoutes);
 app.use('/backend', repartidorRoutes);
 
 app.listen(PORT, () => {
