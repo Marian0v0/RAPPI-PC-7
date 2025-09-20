66cfc9bd103c762544a86038bb5c59e8a78abb32
GerTral <alejandro3mm@gmail.com>
2025-09-20 15:13:57 -0500

    fix: remove debugger de errores
    por fin funciona el server :'v

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const reporteRoutes = require('./routes/reporteRoutes');
-//const clienteRoutes = require('./routes/client.routes.js');
-
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -17,12 +15,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/backend', reporteRoutes);
-
-// Manejo de errores global
-app.use((err, req, res, next) => {
-  console.error('Error global:', err);
-  res.status(500).json({ error: err.message });
-});
 
 app.listen(PORT, () => {
   console.log(`Microservicio ejecutándose en puerto ${PORT}`);
--- backend/utils/database.js
+++ backend/utils/database.js
@@ -9,7 +9,4 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
-console.log('SUPABASE_KEY:', process.env.SUPABASE_KEY);
-
 module.exports = supabase;
