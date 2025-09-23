c34686e905c34374ca9d74cc3dda11da6cb0abbd
LunaBeltran <labeltran222@umariana.edu.co>
2025-09-23 08:16:43 -0500

    Update app.jsx
    correccion coma

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -43,7 +43,7 @@ const router = createBrowserRouter([
   {
     path: "/restaurantes/:nombreRestaurante",
     element: <VRestaurante/>,
-  }
+  },
   {
     path: "/perfil",
     element: <VistaCuenta />,
