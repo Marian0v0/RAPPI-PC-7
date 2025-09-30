5ad844ea6eead80dfd132f56b38c9ed587eb0ff7
Atrallor <142526037+Atrallor@users.noreply.github.com>
2025-09-30 07:50:11 -0500

    fix: mismatch URL to handle login backend request

--- frontend/src/pages/ClienteLogin/ClienteLogin.jsx
+++ frontend/src/pages/ClienteLogin/ClienteLogin.jsx
@@ -27,7 +27,7 @@ const ClienteLogin = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:3000/backend/login", {
+      const response = await fetch("https://rappi-pc-7.onrender.com/backend/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -116,7 +116,7 @@ const ClienteLogin = () => {
             </button>
           </form>
           <button 
-            className="base-button back-button"
+            className="base-button button-back"
             onClick={() => window.history.back()}
             disabled={loading}
           >
